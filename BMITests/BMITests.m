da9d93da832c91892d5763541b0a27de300623fe
João Lucas <joaolucasfp2001@gmail.com>
2020-08-13 23:25:29 -0300

    Added unit test

--- BMITests/BMITests.m
+++ BMITests/BMITests.m
@@ -7,15 +7,18 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "BMIModel.h"
 
 @interface BMITests : XCTestCase
+
+@property BMIModel *bmiModel;
 
 @end
 
 @implementation BMITests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    _bmiModel = [[BMIModel alloc] init];
 }
 
 - (void)tearDown {
@@ -23,15 +26,12 @@
 }
 
 - (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    double weight = 50;
+    double height = 1.50;
+    
+    double result = [_bmiModel resultBMI: weight : height];
+    
+    XCTAssertEqual(result, 22.22222222222222);
 }
 
 @end
